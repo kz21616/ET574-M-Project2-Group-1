508348a441b14f6e8e03c879bc48c2f209ee8452
kz21616 <kz21616@gmail.com>
2026-04-14 22:27:15 +0000

    planning

---
+++ my_math.py
@@ -0,0 +1,26 @@
+#function 1 kevin
+
+#pow(x, y): x raised to the power y
+
+
+
+
+#function 2 kevin
+
+
+
+
+
+
+#function 1 amadou
+
+#sqrt(x): Square root of x
+
+
+
+
+
+#function 2 amadou
+
+
+
