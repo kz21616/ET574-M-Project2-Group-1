0a87d318e92b9c2772b7775868f26d3211473ac7
Amadoumosafi <amadoumosafi@gmail.com>
2026-04-23 22:07:10 +0000

    Add basic tests for is_prime and sum_of_digits

--- my_math.py
+++ my_math.py
@@ -7,18 +7,18 @@ def expo(x,y):
     print(f"{x}to the power of {y} is {x**y}")
 
  
-x = float(input("Base: "))
-y = float(input("Exponent: "))
-expo(x,y)
+    x = float(input("Base: "))
+    y = float(input("Exponent: "))
+    expo(x,y)
   
 #function 2 kevin
 
 def hypotonuse(a,b):
     print(f"The hypotenuse is {(a**2 + b**2)**0.5}")
 
-a = float(input("Side A is: "))
-b = float(input("Side B is: "))
-hypotonuse(a,b)
+    a = float(input("Side A is: "))
+    b = float(input("Side B is: "))
+    hypotonuse(a,b)
 
 #function 1 amadou
 
