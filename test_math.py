7d903a9ce2941c9eb6d70516ce1e4a27de472ad7
kz21616 <kz21616@gmail.com>
2026-04-24 16:01:24 +0000

    Fixed first test, minor mistakes

--- test_math.py
+++ test_math.py
@@ -3,7 +3,7 @@ from my_math import expo, hypotonuse, is_prime, sum_of_digits
 
 #Kevin test:
 
-class test_prime:
+class test_prime(unittest.TestCase):
     def test_one(self):
         self.assertFalse(is_prime(1))  
 
@@ -14,7 +14,7 @@ class test_prime:
         self.assertFalse(is_prime(-4))
 
     def test_prime(self):
-        self.assertTrue(isprime(7))
+        self.assertTrue(is_prime(7))
 
     def test_positive(self):
         self.assertFalse(is_prime(10))
