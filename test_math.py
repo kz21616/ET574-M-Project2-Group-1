d62563fa9fd5c687b17d47db70eb316bf1c0ae67
kz21616 <kz21616@gmail.com>
2026-04-24 15:40:26 +0000

    is prime tests

--- test_math.py
+++ test_math.py
@@ -1,5 +1,20 @@
+import unittest
+from my_math import expo, hypotonuse, is_prime, sum_of_digits
+
 #Kevin test:
-from my_math import expo
+
+class test_prime:
+    def test_one(self):
+        self.assertFalse(self.is_prime(1))  
+
+    def test_zero(self):
+        self.assertFalse(self.isprime(0))  
+
+
+
+
+
+
 
 expo(2,3)
 
@@ -10,3 +25,13 @@ expo(2,3)
 
 
 #Amadou test:
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main
