e936b5c7c6fb56568bc89fa20bfa30bba05db85f
Amadoumosafi <amadoumosafi@gmail.com>
2026-04-24 00:04:54 +0000

    Update test_math to follow requirement (test teammate functions only)

--- test_math.py
+++ test_math.py
@@ -11,15 +11,3 @@
 
 
 #Amadou test:
-from my_math import is_prime, sum_of_digits
-
-# is_prime tests
-print(is_prime(7))   
-print(is_prime(2))    
-print(is_prime(-5))   
-
-
-# sum_of_digits tests
-print(sum_of_digits(123))   
-print(sum_of_digits(0))     
-print(sum_of_digits(-45)) 
